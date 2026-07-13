f450e8f23986f94977daec012056ceaeb9092dd1
Michelle Xu <m.xu711@gmail.com>
2026-07-13 12:46:36 -0500

    Edited cookies

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -101,9 +101,9 @@ export const sessionMiddleware = session({
   saveUninitialized: false,
   name: "courseplanner.sid",
   cookie: {
-    maxAge: 24 * 60 * 60 * 1000, // 24 hours
-    secure: NODE_ENV === "production",
-    sameSite: "lax",
+  maxAge: 24 * 60 * 60 * 1000,
+  secure: true,
+  sameSite: "none",
   },
 });
 
